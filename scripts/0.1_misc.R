c65259c4f8e814a948087540b315f98925c60de0
beausoleilmo <beausoleilmo@gmail.com>
2020-02-12 13:49:54 -0500

    Added known.stat.cjs function

--- scripts/0.1_misc.R
+++ scripts/0.1_misc.R
@@ -76,3 +76,16 @@ autopairs<-function(x, ...) {
         upper.panel = panel.cor, 
         diag.panel = panel.hist, ...)
 }
+
+
+known.state.cjs <- function(ch){
+  state <- ch
+  for (i in 1:dim(ch)[1]){
+    n1 <- min(which(ch[i,]==1))
+    n2 <- max(which(ch[i,]==1))
+    state[i,n1:n2] <- 1
+    state[i,n1] <- 1
+  }
+  state[state==0] <- 0
+  return(state)
+}
