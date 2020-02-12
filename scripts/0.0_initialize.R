98e0a9c67debcd11cb002eb87d5d6c1cca51abbb
beausoleilmo <beausoleilmo@gmail.com>
2020-02-12 13:49:44 -0500

    Added title and sourcing scripts

--- scripts/0.0_initialize.R
+++ scripts/0.0_initialize.R
@@ -1,3 +1,11 @@
+##########################################################################################
+# Created by Marc-Olivier Beausoleil
+# McGill University 
+# Created Tuesday, February 12, 2020 
+# Why: 
+# Requires 
+# NOTES: 
+##########################################################################################
 
 # Load libraries ----------------------------------------------------------
 
@@ -11,7 +19,9 @@ library(vegan)
 
 
 # Source scripts ----------------------------------------------------------
-source('./scripts/analyses.R')
-source('./scripts/misc.R')
-source('./scripts/prep.data.R')
-source("./scripts/Loading data.R")
+source('scripts/0.1_misc.R')
+# Load functions 
+source('./scripts/PCA_custom.R')
+source("./scripts/logit.r")
+# Prepare data script 
+source('scripts/prep.data.R')
