481cc4e520c3e2b4c8409376694d490d570deb6b
beausoleilmo <beausoleilmo@gmail.com>
2020-02-17 18:15:09 -0500

    Made the script work alone!

--- scripts/Capture performance per day.R
+++ scripts/Capture performance per day.R
@@ -1,3 +1,4 @@
+source("scripts/1.0_subsetting_dataset_bird.R")
 plot(table(datata$Date), type = "l", lwd = .25, xaxt = "n", ylab = "Number of catches at each day")
 at = seq(1,length(table(datata$Date)),by = 25)
 axis(1, at = at, labels = names(table(datata$Date))[at],las =3, cex.axis = .9)
@@ -6,7 +7,7 @@ sorted.catches = sort(table(datata$Date), decreasing = TRUE)
 sorted.catches.mob = sort(table(droplevels(datata[datata$By. %in% "MOB","Date"])), decreasing = TRUE)
 
 plot(sorted.catches, 
-     xlim = c(0,100),
+     # xlim = c(0,100),
      type = "l", lwd = 1, xaxt = "n", ylab = "Number of catches at each day")
 at = seq(1,length(sorted.catches),by = 1)
 axis(1, at = at, labels = names(sorted.catches)[at],las =3, cex.axis = .8)
